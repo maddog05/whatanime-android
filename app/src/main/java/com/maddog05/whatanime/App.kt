5ba5363f4d13adf921b4de87944be7febd61fbce
andreetorres <andree.torres.m@gmail.com>
2018-12-31 11:12:10 -0500

    FIX compilation issues

--- app/src/main/java/com/maddog05/whatanime/App.kt
+++ app/src/main/java/com/maddog05/whatanime/App.kt
@@ -2,10 +2,8 @@ package com.maddog05.whatanime
 
 import android.app.Application
 import android.support.v7.app.AppCompatDelegate
-import com.crashlytics.android.Crashlytics
 import com.maddog05.maddogutilities.logger.Logger2
 import com.maddog05.whatanime.core.Logic
-import io.fabric.sdk.android.Fabric
 
 /*
  * Created by andreetorres on 23/12/17.
@@ -14,7 +12,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Logic.get(this).databaseStart(this);
-        Fabric.with(this, Crashlytics())
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         Logger2.get().setEnabled(BuildConfig.IS_LOGGER_ENABLED)
     }

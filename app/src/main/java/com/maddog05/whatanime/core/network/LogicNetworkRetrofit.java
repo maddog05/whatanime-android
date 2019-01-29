8b2a285ed1370892abb9dae17f645601ca985645
andreetorres <andree.torres.m@gmail.com>
2019-01-28 21:05:59 -0500

    CHANGE token

--- app/src/main/java/com/maddog05/whatanime/core/network/LogicNetworkRetrofit.java
+++ app/src/main/java/com/maddog05/whatanime/core/network/LogicNetworkRetrofit.java
@@ -94,7 +94,7 @@ public class LogicNetworkRetrofit implements LogicNetwork {
         data.addProperty("image", fixBase64Input + encodedImage);
         if (services != null) {
             //, filter
-            services.search(TOKEN, data).enqueue(new retrofit2.Callback<JsonObject>() {
+            services.search(null, data).enqueue(new retrofit2.Callback<JsonObject>() {
                 @Override
                 public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                     Logger2.get().d(TAG, "searchWithPhoto: onResponse: httpCode = " + response.code());
@@ -144,7 +144,7 @@ public class LogicNetworkRetrofit implements LogicNetwork {
     public void getQuota(Context context, Callback<Pair<String, OutputGetQuota>> callback) {
         logStart("getQuota");
         if (services != null) {
-            services.getQuota(TOKEN).enqueue(new retrofit2.Callback<JsonObject>() {
+            services.getQuota(null).enqueue(new retrofit2.Callback<JsonObject>() {
                 @Override
                 public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                     Logger2.get().d(TAG, "getQuota: onResponse: httpCode = " + response.code());
